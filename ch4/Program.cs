77a65154b1f8a69302bcf4372415f02f5102025d
aminov <alami@tshtt.uz>
2021-12-27 17:19:51 +0500

    ch4_2 new page -n index (razor)

--- ch4/Program.cs
+++ ch4/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+//app.MapGet("/", () => "Hello World!");
+app.MapRazorPages();
 app.Run();
